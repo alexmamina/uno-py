8db74cef78a271b7c23541539a4747f0872b256e
alexmamina <alexmamina@gmail.com>
2021-05-21 11:11:10 +0100

    Added setup files

---
+++ setup-win.bat
@@ -0,0 +1,7 @@
+#Python (+tkinter), pip, pillow, tkmacosx
+py -m pip install --upgrade pip &&
+py -m pip install --upgrade Pillow &&
+py -m pip install tkmacosx &&
+ECHO "SETUP COMPLETE"
+ECHO "YOU CAN START THE GAME"
+PAUSE
