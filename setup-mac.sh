df2ccda7018317e4ab41e58b83f535c9f8543885
alexmamina <alexmamina@gmail.com>
2024-03-23 20:45:32 +0000

    remove files i've moved to folders

--- setup-mac.sh
+++
@@ -1,9 +0,0 @@
-
-
-#Python (+tkinter), pip, pillow, tkmacosx
-python3 -m pip install --upgrade pip &&
-python3 -m pip install --upgrade Pillow &&
-python3 -m pip install tkmacosx &&
-echo "SETUP COMPLETE" &&
-echo "YOU CAN START THE GAME"
-
