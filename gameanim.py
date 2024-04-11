63d31c1e4fb7320970b674d952383efe3ceaead5
alexmamina <alexmamina@gmail.com>
2024-04-11 21:08:27 +0100

    kind of split up place_card (but it's ui that calls non-ui, so needs more work)

--- gameanim.py
+++ gameanim.py
@@ -397,107 +397,78 @@ class Game(Frame):
     # #################################### EVENTS ##################################
     # UI settings (if not UI, just call complete_placement)
     def place_card(self, ind: int, binst):
-        if self.challenge:
-            self.challenge.destroy()
-        if self.valid_wild:
-            self.valid_wild.destroy()
-        self.update_idletasks()
         card = self.hand_cards[ind].name
         old_card = self.last_played
         # Same color (0:3), same symbol (3:), black
         same_color = card[0:3] == old_card[0:3]
         same_symbol = card[3:] == old_card[3:]
         if same_color or same_symbol or "bla" in card[0:3]:
-            dest_x = self.last.winfo_x()
-            dest_y = self.last.winfo_y()
-            orig_x = binst.winfo_x()
-            orig_y = binst.winfo_y()
-            dx = dest_x - orig_x
-            dy = dest_y - orig_y
+            # You can't have the "Illegal +4" when placing a card, as you never place it, so don't
+            # handle it
+            self.handle_challenge_button(player=None, destroy=True)
             # If animation is turned on, move, else place on pile straight away
             if self.animated:
+                dest_x = self.last.winfo_x()
+                dest_y = self.last.winfo_y()
+                orig_x = binst.winfo_x()
+                orig_y = binst.winfo_y()
+                dx = dest_x - orig_x
+                dy = dest_y - orig_y
                 self.move(orig_x, orig_y, dx, dy, 0, binst, self.last["image"], ind)
             else:
                 binst.destroy()
                 self.complete_placement(card, ind)
 
     # Set new card on pile, send information, update hand - after animation or straight away
-    # both settings
     def complete_placement(self, card: str, ind: int):
         if "reverse" in card:
             self.is_reversed = not self.is_reversed
-            # UI settings
-            self.direction_l["image"] = self.revdir if self.is_reversed else self.fordir
-            print("reversed")
-
         # Changes the black card to black with a color to show which one to play next
         if "bla" in card[0:3]:
-            picker = Picker(
-                self,
+            new_color = self.pick_option(
                 "New color",
                 "Which one?",
                 ["Red", "Green", "Blue", "Yellow"])
-            new_color = picker.result
             new_color = new_color.lower()[0:3]
             # Get the colored black cards from the deck for ease of transfer
             if "plus" in card:
                 new_color += "plus"
-                card_col = new_color + "four"
-                photocard = self.new_deck.get_special(new_color)
+                special_card = new_color + "four"
             else:
                 new_color += "black"
-                card_col = new_color + "black"
-                photocard = self.new_deck.get_special(new_color)
+                special_card = new_color + "black"
         else:  # Not a black card
-            photocard = self.hand_cards[ind].card_pic
-            card_col = card
+            special_card = card
 
         # Remove card from "hand", update label with number
         self.hand_cards.pop(ind)
         self.all_nums_of_cards[self.identity] -= 1
 
-        # UI settings
-        self.hand_btns.pop(ind)
-        self.cards_left.config(text=self.all_nums_of_cards[self.identity])
-        ctr = 0
-        for i in self.hand_btns.keys():
-            # Move all buttons
-            b = self.hand_btns[i]
-            coords = self.get_card_placement(len(self.hand_btns), ctr)
-            b.place(x=coords[1], y=coords[2])
-            ctr += 1
+        self.update_labels_buttons_card_placed(special_card, ind)
 
-        # Non-UI settings
         data_to_send = {
             "played": card,
             "pile": self.pile,
             "stage": Stage.GO,
-            "color": card_col,
+            "color": special_card,
             "num_left": len(self.hand_cards)
         }
         if "plusfour" in card:
             data_to_send["wild"] = self.can_put_plusfour()
         # If placed plustwo in the mode, send the counter
         elif "two" in card and self.modes.stack:
-            self.stack_label.config(text="Stack\n cards to take:\n" + str(
-                self.stack_counter + 2))
-
-            # Non-UI settings
             data_to_send["counter"] = self.stack_counter + 2
             self.stack_counter = 0
         if "7" in card and self.modes.sevenzero and len(self.hand_cards) > 0:
             self.send_design_update(0, len(self.hand_cards), card)
             players = [x for x in self.peeps if not self.peeps.index(x) == self.identity]
-            # Both settings
-            swap = Picker(
-                self,
+            swap_with = self.pick_option(
                 "Swap",
                 "Who would you like to swap your cards with?",
                 players)
-            data_to_send["swapwith"] = self.peeps.index(swap.result)
+            data_to_send["swapwith"] = self.peeps.index(swap_with)
             data_to_send["hand"] = [self.hand_cards[c].name for c in self.hand_cards]
 
-        # Non-UI settings
         if "0" in card and self.modes.sevenzero and len(self.hand_cards) > 0:
             print("Zero")
             data_to_send["hand"] = [self.hand_cards[c].name for c in self.hand_cards]
@@ -509,13 +480,25 @@ class Game(Frame):
             self.sendInfo(data_to_send)
         else:
             self.sendFinal(data_to_send)
+        self.last_played = special_card
 
-        img = ImageTk.PhotoImage(photocard)
-        self.last.config(image=img, text=card_col)
-        self.last["image"] = img
-        self.last.__setattr__("image", img)
-        self.last["text"] = card_col
-        self.last_played = card_col
+        self.update_last_played_img(special_card)
+
+    # UI settings
+    def update_labels_buttons_card_placed(self, card: str, ind: int):
+        if "reverse" in card:
+            self.direction_l["image"] = self.revdir if self.is_reversed else self.fordir
+        self.hand_btns.pop(ind)
+        self.cards_left.config(text=self.all_nums_of_cards[self.identity])
+        self.move_buttons()
+        if "two" in card and self.modes.stack:
+            self.stack_label.config(text="Stack\n cards to take:\n" + str(
+                self.stack_counter + 2))
+
+    # UI settings
+    def pick_option(self, title: str, question: str, options: list[str]) -> str:
+        picker = Picker(self, title, question, options)
+        return picker.result
 
     def take_card(self):
         global message
@@ -593,12 +576,15 @@ class Game(Frame):
         b["command"] = lambda ind=index, binst=b: self.place_card(ind, binst)
         b.__setattr__("image", photo)
         self.hand_btns[index] = b
-        ctr = 0
         if move_is_possible and \
             (self.card_counter == 0 or (self.modes.mult and self.card_counter > 6)) and \
                 self.stack_counter == 0:
             self.new_card.config(state="disabled")
             b.config(state="normal")
+        self.move_buttons()
+
+    def move_buttons(self):
+        ctr = 0
         for i in self.hand_btns.keys():
             # Move all buttons
             b = self.hand_btns[i]
