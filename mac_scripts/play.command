dd2159ef8e7ddc24746552a0a80bf85bda440c3f
alexmamina <alexmamina@gmail.com>
2024-04-22 20:49:47 +0100

    make the update_checker 3.9-compatible and enable auto-updates for mac (based on a folder last edit time)

--- mac_scripts/play.command
+++ mac_scripts/play.command
@@ -2,5 +2,8 @@ cd
 cd Downloads/uno-py-master
 #cd
 #cd Uno
+
+python3.9 update_checker.py
+
 python3 client.py
 echo "Full logs are located in Downloads/uno-py-master/<name>-logs-<date>.log"
--- update_checker.py
+++ update_checker.py
@@ -5,11 +5,19 @@ import subprocess
 command = "curl -s 'https://api.github.com/repos/alexmamina/uno-py/commits'"
 response_blob = subprocess.check_output(command, shell=True)
 response = json.loads(response_blob)
-dates = [datetime.fromisoformat(x['commit']['author']['date']) for x in response]
+dates = [
+    datetime.strptime(
+        x['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ"
+    ).replace(tzinfo=timezone.utc)
+    for x in response
+]
+# Python 3.11:
+# dates = [datetime.fromisoformat(x['commit']['author']['date']) for x in response]
 sorted(dates)
 latest_commit = dates[0]
 
-file_update = datetime.fromtimestamp(os.path.getmtime("update_checker.py")).astimezone(timezone.utc)
+# Get the current file's last modification time - should be the last download time
+file_update = datetime.fromtimestamp(os.path.getmtime("mac_scripts")).astimezone(timezone.utc)
 print(f"This file was last updated on {file_update} UTC")
 
 utc_now = datetime.now().astimezone(timezone.utc)
@@ -17,11 +25,10 @@ print(f"Now it's {utc_now}")
 print(f"Latest update to the project was: {latest_commit} UTC")
 
 if file_update < latest_commit:
-    print("There's an update likely available!"
-          " If you want fewer bugs, more bugs, no changes at all, or cool new features, please"
-          " follow your usual update process to get new files")
+    print("There's an update likely available! Trying...")
+    download_command = "cd && cd Downloads && " + \
+        "wget https://github.com/alexmamina/uno-py/archive/refs/heads/master.zip " + \
+        "-O uno-py-master.zip && unzip -o uno-py-master.zip && rm uno-py-master.zip"
+    subprocess.call(download_command, shell=True)
 else:
     print("Seems up to date")
-
-
-# https://github.com/alexmamina/uno-py/archive/refs/heads/master.zip
